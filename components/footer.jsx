0809e49b1f621cdc1ad65571951985f78529bdc5
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2023-12-26 22:48:16 +0100

    Responsive adjustments

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
         <footer>
-
+            <h3>Get in touch</h3>
+            <h3>Reach me on my socials</h3>
         </footer>
     );
 };
--- components/header.jsx
+++ components/header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 
     return (
         <header className={"header"}>
-            <div class="container mx-auto">
+            <div className="container mx-auto">
                 <div className={"themes-list"}>
                     {themes.map((t,i) => {
                         return (
