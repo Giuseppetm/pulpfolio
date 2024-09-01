14e43ea0bdfc135bd59822b0c9cd8e5bc98799b1
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2024-09-01 12:23:02 +0200

    Update hero

--- sections/hero.jsx
+++ sections/hero.jsx
@@ -38,8 +38,8 @@ const Hero = () => {
                     <div className="grid grid-cols-12 gap-4">
                         <div className="col-span-12 xl:col-span-8 description">
                             <p>
-                                <b className={"accent"}>Passionate Frontend Developer</b> at <b><a href="https://www.peoplelab.it/">People Lab Srl</a></b> based in Milan,
-                                currently working for <b><a href="https://protocube.it/">Protocube Reply</a></b> on software development for the management of 3D models.
+                                <b className={"accent"}>Passionate Frontend Developer</b> at <b><a href="https://www.deloittedigital.com/it/en.html">Deloitte Digital</a></b> based in Milan,
+                                currently focused on e-commerce contexts.
                             </p>
 
                             <p>
@@ -50,7 +50,8 @@ const Hero = () => {
                         <div className="col-span-2 col-start-11 work-details-col">
                             <div className={"work-details"}>
                                 <p className={"accent"}>Worked for</p>
-                                {/*<p><a href="https://protocube.it/">Protocube Reply /</a></p>*/}
+                                <p><a href="https://www.peoplelab.it/">People Lab /</a></p>
+                                <p><a href="https://protocube.it/">Protocube Reply /</a></p>
                                 <p><a href="https://whirlpoolcorp.com/">Whirlpool /</a></p>
                                 <p><a href="https://keycrime.com/">Keycrime /</a></p>
                                 <p><a href="https://www.egeatech.com/">Egea Tecnologie Informatiche /</a></p>
